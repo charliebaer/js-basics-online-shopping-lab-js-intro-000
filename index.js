4f5f6bc152e057b9483309ef0d565b66fc01b1c4
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 14:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  if (cart.length>0){
+  if(cart.length>0){
     var arr=[];
     for(let i=0;i<cart.length;i++){
     arr.push(cart[i].itemname+" at $"+cart[i].itemPrice)
