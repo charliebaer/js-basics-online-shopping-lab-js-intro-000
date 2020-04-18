7831b10ff591b39cbc827b0ce59792fe767c87bf
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 14:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
  // write your code here
  var obj={itemName: item ,itemPrice:Math.floor(Math.random() * 100) + 1}
  cart.push(obj);
- return(`${item} has been added to your cart`)
+ return(`${item} has been added to your cart.`)
 }
 
 function viewCart() {
