f26f9b6f851b87afb386d9d7fd5580e883b4cbe1
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 17:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-
+  for(let i=0;i<cart.length;i++){
+    if(cart[i].itemName === item ){
+      return(cart.splice(i,1));
+    }
+    else return("That item is not in your cart.")
+  }
 }
 
 function placeOrder(cardNumber) {
