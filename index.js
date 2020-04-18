79acc0d3a4119a45b221d255c6a396c29d7ff349
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 18:23:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(let i=0;i<cart.length;i++){
-    if(cart[i].itemName === item ){
+    if( item === cart[i].itemName){
       cart.splice(i,1);
       return(cart);
     }
